747424b2fb6d986a566708a8b67b7a30f44ea3c9
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-16 01:16:30 -0500

    Menu , contenido y footer

---
+++ views/modulos/footer.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2019 
+        <a href="https://www.oversistemas.com" target="_blank">Oversistemas</a>.
+    </strong>
+
+    Todos los derechos reservados.
+</footer>
