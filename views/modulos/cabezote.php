c870a68300f2ac4d6c496cbae9320f8e675e62dc
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-15 23:49:28 -0500

    Cabezote

---
+++ views/modulos/cabezote.php
@@ -0,0 +1,59 @@
+<header class="main-header">
+
+    <!-- //
+    // ──────────────────────────────────────────────────────── ──────────
+    // :::::: L O G O T I P O : : : : : : : :
+    // ──────────────────────────────────────────────────────────────────
+    // -->
+
+    <a href="#" class="logo">
+        <!-- logo mini NOTE modulo config -->
+        <span class="logo-mini">
+            <img src="views/img/plantilla/icono-blanco.png" class="img-responsive" style="padding:10px">
+        </span>
+
+        <!-- logo normal NOTE modulo config -->
+        <span class="logo-lg">
+            <img src="views/img/plantilla/logo-blanco-lineal.png" class="img-responsive" style="padding:10px 0">
+        </span>
+    </a>
+
+    <!-- //
+    // ──────────────────────────────────────────────────────── ──────────
+    // :::::: BARRA DE NAVEGACION : : : : : : : :
+    // ──────────────────────────────────────────────────────────────────
+    // -->
+    <nav class="navbar navbar-static-top" role="navigation">
+
+        <!-- Boton de navegación -->
+        <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+            <span class="sr-only">Toggle navigation</span>
+        </a>
+
+        <!-- Perfil de usuario -->
+        <div class="navbar-custom-menu">
+
+            <ul class="nav navbar-nav">
+                <li class="dropdown user user-menu">
+                    <a href="http://" class="dropdown-togl">
+                        <img src="views/img/usuarios/default/anonymous.png" class="user-image">
+                        <!--NOTE modulo config-->
+                        <span class="hidden-xs">Usuario Administrador</span>
+                    </a>
+                    <!-- Dropdown-toggle -->
+                    <ul class="dropdown-menu">
+                        <li class="user-body">
+                            <div class="pull-right">
+                                <a href="salir" class="btn btn-default btn-flat">Salir</a>
+                            </div>
+                        </li>
+
+                    </ul>
+                </li>
+            </ul>
+
+        </div> <!-- fin Perfil de usuario -->
+
+    </nav>
+
+</header>
