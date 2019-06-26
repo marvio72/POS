b424f7b0e2b77c7129aa3c7f00be6242316e399d
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-06-26 18:58:14 -0500

    Maquetando el módulo de usuarios

--- views/modulos/usuarios.php
+++ views/modulos/usuarios.php
@@ -1,45 +1,214 @@
 <div class="content-wrapper">
 
-<section class="content-header">
-    <h1>
-        Administrar Usuarios
+    <section class="content-header">
+        <h1>
+            Administrar Usuarios
 
-    </h1>
+        </h1>
 
-    <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Inicio</a></li>
-        <!-- <li><a href="#">Examples</a></li> -->
-        <li class="active">Administrar Usuarios</li>
-    </ol>
+        <ol class="breadcrumb">
+            <li><a href="inicio"><i class="fa fa-dashboard"></i> Inicio</a></li>
+            <!-- <li><a href="#">Examples</a></li> -->
+            <li class="active">Administrar Usuarios</li>
+        </ol>
 
-</section> <!--Fin .content-header-->
+    </section>
+    <!--Fin .content-header-->
 
-<!-- Main content -->
-<section class="content">
+    <!-- Main content -->
+    <section class="content">
 
-    <!-- Default box -->
-    <div class="box">
-        <div class="box-header with-border">
-            <h3 class="box-title">Title</h3>
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header with-border">
 
-            <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
-                    <i class="fa fa-minus"></i></button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-                    <i class="fa fa-times"></i></button>
+                <button class="btn btn-primary" data-toggle="modal" data-target="#modalAgregarUsuario">
+                    Agregar usuario
+                </button>
+
+
             </div>
-        </div>
-        <div class="box-body">
-            Start creating your amazing application!
-        </div>
-        <!-- /.box-body -->
-        <div class="box-footer">
-            Footer
-        </div>
-        <!-- /.box-footer-->
-    </div>
-    <!-- /.box -->
+            <div class="box-body">
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Nombre</th>
+                            <th>Usuario</th>
+                            <th>Foto</th>
+                            <th>Perfil</th>
+                            <th>Estado</th>
+                            <th>Último login</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Usuario Administrador</td>
+                            <td>admin</td>
+                            <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                            <td>Administrador</td>
+                            <td><button class="btn btn-success btn-xs">Activado</button></td>
+                            <td>2017-12-11 12:05:32</td>
+                            <td>
+                                <div class="bt-group">
+                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Usuario Administrador</td>
+                            <td>admin</td>
+                            <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                            <td>Administrador</td>
+                            <td><button class="btn btn-success btn-xs">Activado</button></td>
+                            <td>2017-12-11 12:05:32</td>
+                            <td>
+                                <div class="bt-group">
+                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Usuario Administrador</td>
+                            <td>admin</td>
+                            <td><img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                            <td>Administrador</td>
+                            <td><button class="btn btn-danger btn-xs">Desactivado</button></td>
+                            <td>2017-12-11 12:05:32</td>
+                            <td>
+                                <div class="bt-group">
+                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <!-- /.box-body -->
 
-</section>
-<!-- /.content -->
+        </div>
+        <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+</div>
+
+<!--***************   *** Comentario *** ****************
+/* MODAL AGREGAR USUARIO
+/*****************   *** ********** *** ****************-->
+<!-- MODAL -->
+<div id="modalAgregarUsuario" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+
+        <div class="modal-content">
+            <form role="form" method="post" enctype="multipart/form-data">
+                <!--***************   *** Comentario *** ****************
+            /* CABEZA DEL MODAL
+            /*****************   *** ********** *** ****************-->
+
+                <div class="modal-header" style="background:#3c8dbc; color:white">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Agregar Usuario</h4>
+                </div>
+                <!--***************   *** Comentario *** ****************
+            /* CUERPO DEL MODAL
+            /*****************   *** ********** *** ****************-->
+
+                <div class="modal-body">
+                    <div class="box-body">
+                        <!--NOMBRE-->
+                        <div class="form-group">
+
+                            <div class="input-group">
+
+                                <span class="input-group-addon"><i class="fa fa-user"></i></span>
+
+                                <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Nombre">
+
+                            </div>
+
+                        </div>
+                        <!--USUARIO-->
+                        <div class="form-group">
+
+                            <div class="input-group">
+
+                                <span class="input-group-addon"><i class="fa fa-key"></i></span>
+
+                                <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Ingresar Usuario">
+
+                            </div>
+
+                        </div>
+                        <!--CONTRASEÑA-->
+                        <div class="form-group">
+
+                            <div class="input-group">
+
+                                <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+
+                                <input type="password" class="form-control input-lg" name="nuevoPassword" placeholder="Ingresar Contraseña">
+
+                            </div>
+
+                        </div>
+                        <!--PERFIL-->
+                        <div class="form-group">
+
+                            <div class="input-group">
+
+                                <span class="input-group-addon"><i class="fa fa-users"></i></span>
+
+                                <select class="form-control input-lg" name="nuevoPerfil">
+
+                                    <option value="">Seleccionar Perfil</option>
+
+                                    <option value="Administrador">Administrador</option>
+
+                                    <option value="Especial">Especial</option>
+
+                                    <option value="Vendedor">Vendedor</option>
+
+                                </select>
+
+                            </div>
+
+                        </div>
+                        <!--ENTRADA PARA SUBIR FOTO-->
+                        <div class="form-group">
+
+                            <div class="panel">SUBIR FOTO</div>
+
+                            <input type="file" name="nuevaFoto" id="nuevaFoto">
+
+                            <p class="help-block">Peso máximo de la foto 200 MB</p>
+
+                            <img src="views/img/usuarios/default/anonymous.png" class="img-thumbnail" width="100px">
+
+                        </div>
+
+                    </div>
+                </div>
+                <!--***************   *** Comentario *** ****************
+            /* PIE DEL MODAL
+            /*****************   *** ********** *** ****************-->
+
+                <div class="modal-footer">
+
+                    <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+                    <button type="submit" class="btn btn-primary">Guardar usuario</button>
+
+                </div>
+            </form>
+        </div>
+
+    </div>
 </div>
