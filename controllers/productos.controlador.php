cfac07c6b24513a2e6e3810db8ee15964f786224
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-07-25 18:57:39 -0500

    Seleccionar Productos

--- controllers/productos.controlador.php
+++ controllers/productos.controlador.php
@@ -1,4 +1,18 @@
 <?php 
  
+class ControladorProductos{
 
+    /*====================Comentario====================
+    MOSTRAR PRODUCTOS
+    ==================================================*/
+
+    public static function ctrMostrarProductos($item, $valor){
+
+        $tabla = "productos";
+
+        $respuesta = ModeloProductos::mdlMostrarProductos($tabla, $item, $valor);
+
+        return $respuesta;
+    }
+}
  
--- views/plantillas/productos.html.php
+++ views/plantillas/productos.html.php
@@ -46,43 +46,57 @@
                         </tr>
                     </thead>
                     <tbody>
-
-                    <?php
-                    
-                        $item = null;
-                        $valor = null;
-
-                        $productos = ControladorProductos::ctrMostrarProductos($item,$valor);
-
-                        foreach ($productos as $key => $value) {
-                            
-                            echo '<tr>
-                                        <td>'.($key + 1).'</td>
-                                        <td><img src="views/img/Productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-                                        <td>'.$value["codigo"].'</td>
-                                        <td>'.$value["descripcion"].'</td>';
-
-                                        $item = "id";
-                                        $valor = $value["id_categoria"];
-
-                                        $categoria = ControladorCategorias::ctrMostrarCategorias($item, $valor);
-
-                                        echo '<td>'.$categoria["categoria"].'</td>
-                                        <td>'.$value["stock"].'</td>
-                                        <td>'.$value["precio_compra"].'</td>
-                                        <td>'.$value["precio_venta"].'</td>
-                                        <td>'.$value["fecha"].'</td>
-                                        <td>
-                                            <div class="bt-group">
-                                                <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-                                                <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-                                            </div>
-                                        </td>
-                                    </tr>';
-                        }
-                    
-                    ?>
-                       
+                        <tr>
+                            <td>1</td>
+                            <td><img src="views/img/Productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                            <td>0001</td>
+                            <td>Aspiradora Industrial</td>
+                            <td>Equipos Electromecánicos</td>
+                            <td>20</td>
+                            <td>150</td>
+                            <td>210</td>
+                            <td>2017-12-11 12:05:32</td>
+                            <td>
+                                <div class="bt-group">
+                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td><img src="views/img/Productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                            <td>0001</td>
+                            <td>Aspiradora Industrial</td>
+                            <td>Equipos Electromecánicos</td>
+                            <td>20</td>
+                            <td>150</td>
+                            <td>210</td>
+                            <td>2017-12-11 12:05:32</td>
+                            <td>
+                                <div class="bt-group">
+                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td><img src="views/img/Productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
+                            <td>0001</td>
+                            <td>Aspiradora Industrial</td>
+                            <td>Equipos Electromecánicos</td>
+                            <td>20</td>
+                            <td>150</td>
+                            <td>210</td>
+                            <td>2017-12-11 12:05:32</td>
+                            <td>
+                                <div class="bt-group">
+                                    <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
+                                    <button class="btn btn-danger"><i class="fa fa-times"></i></button>
+                                </div>
+                            </td>
+                        </tr>
 
                     </tbody>
                 </table>
