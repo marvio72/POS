a559eb0de33aa2c4099b13e35c549bdca6d5c82a
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-08-05 18:49:13 -0500

    Operación de stockmax y min para crear una alerta en stock

--- views/modulos/productos.php
+++ views/modulos/productos.php
@@ -38,6 +38,8 @@
                             <th>Descripción</th>
                             <th>Categoría</th>
                             <th>Stock</th>
+                            <th>S+</th>
+                            <th>S-</th>
                             <th>Precio de compra</th>
                             <th>Precio de venta</th>
                             <th>Agregado</th>
@@ -45,46 +47,7 @@
 
                         </tr>
                     </thead>
-                    <!-- <tbody>
 
-                    <?php
-
-                    // $item = null;
-                    // $valor = null;
-
-                    // $productos = ControladorProductos::ctrMostrarProductos($item, $valor);
-
-                    // foreach ($productos as $key => $value) {
-
-                    //     echo '<tr>
-                    //                     <td>' . ($key + 1) . '</td>
-                    //                     <td><img src="views/img/Productos/default/anonymous.png" class="img-thumbnail" width="40px"></td>
-                    //                     <td>' . $value["codigo"] . '</td>
-                    //                     <td>' . $value["descripcion"] . '</td>';
-
-                    //     $item = "id";
-                    //     $valor = $value["id_categoria"];
-
-                    //     $categoria = ControladorCategorias::ctrMostrarCategorias($item, $valor);
-
-                    //     echo '<td>' . $categoria["categoria"] . '</td>
-                    //                     <td>' . $value["stock"] . '</td>
-                    //                     <td>' . $value["precio_compra"] . '</td>
-                    //                     <td>' . $value["precio_venta"] . '</td>
-                    //                     <td>' . $value["fecha"] . '</td>
-                    //                     <td>
-                    //                         <div class="bt-group">
-                    //                             <button class="btn btn-warning"><i class="fa fa-pencil"></i></button>
-                    //                             <button class="btn btn-danger"><i class="fa fa-times"></i></button>
-                    //                         </div>
-                    //                     </td>
-                    //                 </tr>';
-                    // }
-
-                    ?>
-                       
-
-                    </tbody> -->
                 </table>
             </div>
             <!-- /.box-body -->
